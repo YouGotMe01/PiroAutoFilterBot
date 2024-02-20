283c8425398e45333121823ad82867bdca9b4a30
YouGotMe01 <139981677+YouGotMe01@users.noreply.github.com>
2024-02-21 02:10:13 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -51,7 +51,7 @@ async def save_file(media):
         )
     except ValidationError:
         logger.exception('Error occurred while saving file in database')
-        return False, 2
+        return None
     else:
         try:
             await file.commit()
@@ -60,12 +60,11 @@ async def save_file(media):
                 f'{getattr(media, "file_size", "NO_FILE")} is already saved in database'
             )
 
-            return False, 0
+            return None
         else:
             logger.info(f'{getattr(media, "file_size", "NO_FILE")} is saved to database')
             caption_text = media.caption.html if media.caption else file_name
-            await add_chnl_message(caption_text)
-            return True, 1
+            return caption_text
 
 
 
