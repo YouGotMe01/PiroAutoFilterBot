2cb534e174dab76d75c3c3dbb4cea4f8df1264ef
YouGotMe01 <139981677+YouGotMe01@users.noreply.github.com>
2024-03-20 20:05:04 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,7 +1,7 @@
 import asyncio
 from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
+from pyrogram.errors import BadRequest
 from info import CHANNELS, UPDATES_CHNL
 from database.ia_filterdb import save_file
 from utils import add_chnl_message, get_poster, temp
@@ -24,17 +24,15 @@ async def media(bot, message):
     if text is not None:
         movie_name, year, languages = await add_chnl_message(text)
         if movie_name is not None:
-            mozhi = " ".join(languages)
-            if mozhi is None:
-                cap = f"<b>#MovieUpdate:\n🧿 <u>𝚃𝙸𝚃𝙻𝙴</u> : <code>{movie_name}</code>\n📆 <u>𝚁𝙴𝙻𝙴𝙰𝚂𝙴</u> : {year}\n\nCopy & Paste In Group To Search\n---»<a href=https://t.me/isaimini_updates/110> ᴍᴏᴠɪᴇ sᴇᴀʀᴄʜɪɴɢ ɢʀᴏᴜᴘ ʟɪɴᴋs </a>«---</b>"
-            elif year is None:
-                cap = f"<b>#MovieUpdate:\n🧿 <u>𝚃𝙸𝚃𝙻𝙴</u> : <code>{movie_name}</code>\n🎙️<u>𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴</u> : {mozhi}\n\nCopy & Paste In Group To Search\n---»<a href=https://t.me/isaimini_updates/110> ᴍᴏᴠɪᴇ sᴇᴀʀᴄʜɪɴɢ ɢʀᴏᴜᴘ ʟɪɴᴋs </a>«---</b>"            
-            else:
-                cap = f"<b>#MovieUpdate:\n🧿 <u>𝚃𝙸𝚃𝙻𝙴</u> : <code>{movie_name}</code>\n📆 <u>𝚁𝙴𝙻𝙴𝙰𝚂𝙴</u> : {year}\n🎙️<u>𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴</u> : {mozhi}\n\nCopy & Paste In Group To Search\n---»<a href=https://t.me/isaimini_updates/110> ᴍᴏᴠɪᴇ sᴇᴀʀᴄʜɪɴɢ ɢʀᴏᴜᴘ ʟɪɴᴋs </a>«---</b>"
+            languages_str = " ".join(languages) if languages else None
             if year is not None:
-                search = f"{movie_name} {year}"
+                caption = f"<b>#MovieUpdate:\n🧿 <u>𝚃𝙸𝚃𝙻𝙴</u> : <code>{movie_name}</code>\n📆 <u>𝚁𝙴𝙻𝙴𝙰𝚂𝙴</u> : {year}\n"
             else:
-                search = f"{movie_name}"
+                caption = f"<b>#MovieUpdate:\n🧿 <u>𝚃𝙸𝚃𝙻𝙴</u> : <code>{movie_name}</code>\n"
+            if languages_str:
+                caption += f"🎙️<u>𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴</u> : {languages_str}\n"
+            caption += "\nCopy & Paste In Group To Search\n---»<a href=https://t.me/isaimini_updates/110> ᴍᴏᴠɪᴇ sᴇᴀʀᴄʜɪɴɢ ɢʀᴏᴜᴘ ʟɪɴᴋs </a>«---</b>"
+            search = f"{movie_name} {year}" if year is not None else movie_name
             movies = await get_poster(search)
             search_with_underscore = search.replace(" ", "_")
             btn = [[
@@ -42,29 +40,26 @@ async def media(bot, message):
             ]]
             markup = InlineKeyboardMarkup(btn)
             if movies and movies.get('poster'):
-                await asyncio.sleep(10)
                 try:
                     await bot.send_photo(
                         chat_id=UPDATES_CHNL,
                         photo=movies.get('poster'),
-                        caption=cap,
+                        caption=caption,
                         reply_markup=markup,
                         parse_mode=enums.ParseMode.HTML
                     )
-                except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
+                except BadRequest as e:
                     await bot.send_message(
                         chat_id=UPDATES_CHNL,
-                        text=cap,
-                        disable_web_page_preview=True,
+                        text=caption,
                         reply_markup=markup,
                         parse_mode=enums.ParseMode.HTML
                     )
             else:
-                await asyncio.sleep(10)
                 await bot.send_message(
                     chat_id=UPDATES_CHNL,
-                    text=cap,
-                    disable_web_page_preview=True,
+                    text=caption,
                     reply_markup=markup,
                     parse_mode=enums.ParseMode.HTML
                 )
+            await asyncio.sleep(5)
